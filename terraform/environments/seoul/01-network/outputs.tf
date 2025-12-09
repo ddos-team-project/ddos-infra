2f6d2dbc5005e0262ef52262e78f8bf3fc11f42f
JinhwanNoh <xsest55@gmail.com>
2025-12-09 09:09:53 +0900

    feat: 서울리전 VPC 구성
    - 서비스 분리를 위해 AZ별 서브넷 4개 구성 (public, private, database, intra)
    - AZ 2개 사용 (ap-northeast-2a, ap-northeast-2c)
    - Transit Gateway 구성으로 리전간 연결 준비
    - NAT Gateway 이중화 (AZ별 1개씩)

---
+++ terraform/environments/seoul/01-network/outputs.tf
@@ -0,0 +1,54 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_cidr" {
+  description = "VPC CIDR block"
+  value       = module.vpc.vpc_cidr_block
+}
+
+output "public_subnets" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  description = "Private subnet IDs (App/EKS)"
+  value       = module.vpc.private_subnets
+}
+
+output "app_subnets" {
+  description = "Application subnet IDs (alias for private_subnets)"
+  value       = module.vpc.private_subnets
+}
+
+output "database_subnets" {
+  description = "Database subnet IDs"
+  value       = module.vpc.database_subnets
+}
+
+output "db_subnets" {
+  description = "Database subnet IDs (alias for database_subnets)"
+  value       = module.vpc.database_subnets
+}
+
+output "intra_subnets" {
+  description = "Intra subnet IDs (TGW, Endpoints)"
+  value       = module.vpc.intra_subnets
+}
+
+output "nat_gateway_ids" {
+  description = "NAT Gateway IDs"
+  value       = module.vpc.natgw_ids
+}
+
+output "tgw_id" {
+  description = "Transit Gateway ID"
+  value       = aws_ec2_transit_gateway.seoul_tgw.id
+}
+
+output "tgw_attachment_id" {
+  description = "Transit Gateway VPC Attachment ID"
+  value       = aws_ec2_transit_gateway_vpc_attachment.seoul_tgw_att.id
+}
