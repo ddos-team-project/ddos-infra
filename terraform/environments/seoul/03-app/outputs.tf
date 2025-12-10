d600c0bd77661f46bbd1f2c6c60fba7a18d6b982
bong-pal <etgt777@naver.com>
2025-12-11 01:26:52 +0900

    ﻿refactor: Route53 가중치/페일오버를 서울 ALB 출력 기반으로 정리
    - 서울 03-app에 ALB zone ID 출력 추가로 alias용 값 노출
    - global/route53이 seoul/03-app/healthcheck-api.tfstate를 참조, 도쿄는 enable_tokyo 플래그로 선택적 사용
    - 가중치(서울80/도쿄20)와 페일오버(서울PRIMARY/도쿄SECONDARY) 구조 유지하며 출력 이름을 healthcheck_alb_*로 맞춤

--- terraform/environments/seoul/03-app/outputs.tf
+++ terraform/environments/seoul/03-app/outputs.tf
@@ -2,6 +2,10 @@ output "healthcheck_alb_dns_name" {
   value = module.healthcheck_api_alb.alb_dns_name
 }
 
+output "healthcheck_alb_zone_id" {
+  value = module.healthcheck_api_alb.alb_zone_id
+}
+
 output "healthcheck_app_sg_id" {
   value = module.healthcheck_api_asg.app_sg_id
 }
--- terraform/global/route53/variables.tf
+++ terraform/global/route53/variables.tf
@@ -1,17 +1,23 @@
 variable "domain_name" {
-  description = "Hosted Zone 도메인"
+  description = "Route53 퍼블릭 호스티드 존 도메인"
   type        = string
   default     = "ddos.io.kr"
 }
 
 variable "api_tier1_record" {
-  description = "Tier1 API 도메인"
+  description = "Tier1 API 레코드(FQDN)"
   type        = string
   default     = "api-tier1.ddos.io.kr"
 }
 
 variable "api_tier2_record" {
-  description = "Tier2 API 도메인"
+  description = "Tier2 API 레코드(FQDN)"
   type        = string
   default     = "api-tier2.ddos.io.kr"
+}
+
+variable "enable_tokyo" {
+  description = "도쿄 리전을 활성화해 가중치/페일오버에 포함할지 여부"
+  type        = bool
+  default     = false
 }
