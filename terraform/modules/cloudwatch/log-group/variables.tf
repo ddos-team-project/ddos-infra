cec9f74f24d599a2347eaf7c6e80d8e7950dd20c
wookddang <yy832484@gmail.com>
2025-12-11 13:14:35 +0900

    docs: CloudWatch 구성
    - CloudWatch Log Group 모듈
    - cwagent-config.json 구성
    - CloudWatch 관련 user-data.tpl 수정 및 추가
    - seoul/02-data 환경 수정

--- terraform/modules/cloudwatch/log-group/variables.tf
+++ terraform/modules/cloudwatch/log-group/variables.tf
@@ -8,13 +8,56 @@ variable "env" {
   type        = string
 }
 
-variable "log_group_names" {
-  description = "생성할 로그 그룹 이름 뒤쪽 목록 (예: [\"healthcheck-app\"])"
-  type        = list(string)
+
+variable "region" {
+  description = "Region (seoul/tokyo)"
+  type        = string
+}
+
+variable "log_groups_by_tier" {
+  description = "티어(t1, t2, db)별 로그 그룹 리스트"
+  type        = map(list(string))
 }
 
 variable "retention_in_days" {
   description = "로그 보존 기간(일 단위)"
   type        = number
   default     = 30
+}
+
+variable "pattern_list" {
+  type        = list(string)
+  description = "Metric Filter 패턴 (예: \"ERROR\")"
+}
+
+variable "metric_namespace" {
+  type        = string
+  description = "Metric 이름 (예: error_count)"
+
+}
+
+
+variable "period" {
+  type    = number
+  default = 60
+}
+
+variable "evaluation_periods" {
+  type    = number
+  default = 1
+}
+
+variable "threshold" {
+  type    = number
+  default = 5
+}
+
+variable "comparison_operator" {
+  type    = string
+  default = "GreaterThanOrEqualToThreshold"
+}
+
+variable "alarm_actions" {
+  type    = list(string)
+  default = []
 }
