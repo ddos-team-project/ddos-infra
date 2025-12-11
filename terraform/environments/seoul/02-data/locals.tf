cdefd8186297ee6cb4a9ef23d65e74768e351105
wookddang <yy832484@gmail.com>
2025-12-11 13:14:35 +0900

    docs: CloudWatch 구성
    - CloudWatch Log Group 모듈
    - cwagent-config.json 구성
    - CloudWatch 관련 user-data.tpl 수정 및 추가
    - seoul/02-data 환경 수정

--- terraform/environments/seoul/02-data/locals.tf
+++ terraform/environments/seoul/02-data/locals.tf
@@ -1,11 +1,11 @@
 locals {
   # 리소스 이름 prefix (지역명 사용)
-  name_prefix = "${var.project}-${var.environment}-${var.tier}-${var.location}"
+  name_prefix = "${var.project}-${var.env}-${var.tier}-${var.location}"
 
   # 공통 태그 (Region은 코드로)
   common_tags = {
     Project   = var.project
-    Env       = var.environment
+    Env       = var.env
     Tier      = var.tier
     Region    = var.region_code # 태그는 apne2
     ManagedBy = "terraform"
