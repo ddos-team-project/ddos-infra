b8349d0441f779d59375115d513af428739ab4d8
JinhwanNoh <xsest55@gmail.com>
2025-12-11 15:29:43 +0900

    feat: Aurora 페일백 자동화 런북 및 Terraform 리소스 추가

--- terraform/environments/seoul/automation/outputs.tf
+++ terraform/environments/seoul/automation/outputs.tf
@@ -1,9 +1,14 @@
 output "ssm_automation_role_arn" {
-  description = "SSM Automation 페일오버용 IAM 역할 ARN"
+  description = "SSM Automation Aurora 페일오버/페일백용 IAM 역할 ARN"
   value       = aws_iam_role.ssm_automation_failover_role.arn
 }
 
-output "ssm_document_name" {
-  description = "생성된 SSM Automation Document 이름"
+output "failover_document_name" {
+  description = "Aurora 페일오버 SSM Automation Document 이름"
   value       = aws_ssm_document.aurora_failover_runbook.name
+}
+
+output "failback_document_name" {
+  description = "Aurora 페일백 SSM Automation Document 이름"
+  value       = aws_ssm_document.aurora_failback_runbook.name
 }
