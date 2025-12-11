e340cf6e8e4ea00d22419d51ff60cee8d699287d
bong-pal <etgt777@naver.com>
2025-12-11 20:58:51 +0900

    fix: route53 중복생성 해결
    - 생성여부 체크 후 진행하도록 변경

--- terraform/global/route53/backend.tf
+++ terraform/global/route53/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "diehard-ddos-tf-state-lock"
     key            = "global/route53/terraform.tfstate"
     region         = "ap-northeast-2"
-    profile        = "admin-role"
+    # profile        = "admin-role"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
