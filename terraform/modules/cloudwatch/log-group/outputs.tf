b5a69ff3d0c8775cdb9cd1994b797f868f89e176
wookddang <yy832484@gmail.com>
2025-12-12 09:11:12 +0900

    feat: 앱 ASG 구조 리팩토링 및 CloudWatch 로그 그룹 모듈 연동

--- terraform/modules/cloudwatch/log-group/outputs.tf
+++ terraform/modules/cloudwatch/log-group/outputs.tf
@@ -1,16 +1,3 @@
-output "log_groups" {
-  value = aws_cloudwatch_log_group.this
-}
 output "log_group_names" {
-  value = [
-    for lg in aws_cloudwatch_log_group.this :
-    lg.name
-  ]
-}
-output "metric_filters" {
-  value = aws_cloudwatch_log_metric_filter.this
-}
-
-output "alarms" {
-  value = aws_cloudwatch_metric_alarm.this
+  value = keys(aws_cloudwatch_log_group.this)
 }
--- terraform/modules/cloudwatch/log-group/variables.tf
+++ terraform/modules/cloudwatch/log-group/variables.tf
@@ -7,16 +7,9 @@ variable "env" {
   description = "환경 이름 (예: poc, prod)"
   type        = string
 }
-
-
 variable "region" {
   description = "Region (seoul/tokyo)"
   type        = string
-}
-
-variable "log_groups_by_tier" {
-  description = "티어(t1, t2, db)별 로그 그룹 리스트"
-  type        = map(list(string))
 }
 
 variable "retention_in_days" {
@@ -25,39 +18,10 @@ variable "retention_in_days" {
   default     = 30
 }
 
-variable "pattern_list" {
-  type        = list(string)
-  description = "Metric Filter 패턴 (예: \"ERROR\")"
+variable "services" {
+  type = list(string)
 }
 
-variable "metric_namespace" {
-  type        = string
-  description = "Metric 이름 (예: error_count)"
-
-}
-
-
-variable "period" {
-  type    = number
-  default = 60
-}
-
-variable "evaluation_periods" {
-  type    = number
-  default = 1
-}
-
-variable "threshold" {
-  type    = number
-  default = 5
-}
-
-variable "comparison_operator" {
-  type    = string
-  default = "GreaterThanOrEqualToThreshold"
-}
-
-variable "alarm_actions" {
-  type    = list(string)
-  default = []
+variable "tier" {
+  type = string
 }
