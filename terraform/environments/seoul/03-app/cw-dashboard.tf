a86b65bf61b91f36c335a6b151d818ea57a35a7e
wookddang <yy832484@gmail.com>
2025-12-13 14:23:05 +0900

    docs: feat: 클라우드와치 대시보드 (서울/도쿄)

--- terraform/environments/seoul/03-app/cw-dashboard.tf
+++ terraform/environments/seoul/03-app/cw-dashboard.tf
@@ -27,12 +27,39 @@ resource "aws_cloudwatch_dashboard" "ddos_prod" {
           metrics = [
             ["AWS/EC2", "CPUUtilization", "AutoScalingGroupName", module.healthcheck_api_asg.autoscaling_group_name],
             ["AWS/EC2", "StatusCheckFailed", "AutoScalingGroupName", module.healthcheck_api_asg.autoscaling_group_name]
-
+          ]
+          period = 60
+        }
+      },
+      {
+        type = "metric",
+        x    = 0, y = 6, width = 12, height = 6,
+        properties = {
+          title  = "ALB Req & 5XX (Tokyo)"
+          view   = "timeSeries"
+          region = "ap-northeast-1"
+          metrics = [
+            ["AWS/ApplicationELB", "RequestCount", "LoadBalancer", "app/healthcheck-api-tokyo-alb/e96ebc5e91cc7975"],
+            [".", "HTTPCode_Target_5XX_Count", ".", "."],
+            [".", "TargetResponseTime", ".", ".", { "stat" : "p95" }]
+          ]
+          period = 60
+        }
+      },
+      {
+        type = "metric",
+        x    = 12, y = 6, width = 12, height = 6,
+        properties = {
+          title  = "EC2 CPU/Status (Tokyo)"
+          view   = "timeSeries"
+          region = "ap-northeast-1"
+          metrics = [
+            ["AWS/EC2", "CPUUtilization", "AutoScalingGroupName", "healthcheck-api-tokyo-asg"],
+            ["AWS/EC2", "StatusCheckFailed", "AutoScalingGroupName", "healthcheck-api-tokyo-asg"]
           ]
           period = 60
         }
       }
-      # 추가: Tokyo/IDC, Aurora 등 필요시 이어서 배치
     ]
   })
 }
